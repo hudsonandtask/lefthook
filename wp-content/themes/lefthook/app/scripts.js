53fbb3fa4737ab2698bd624cc3e8dfb6b44c6aed
hudsonandtask <michael.henryk@gmail.com>
2016-02-29 15:50:21 -0500

    scripts.js change

--- wp-content/themes/lefthook/app/scripts.js
+++ wp-content/themes/lefthook/app/scripts.js
@@ -89,6 +89,35 @@
 
     }
 })();
+(function () {
+    angular
+        .module('lefthook')
+        .controller('homeCtrl', homeCtrl);
+
+    homeCtrl.$inject = [
+        'wpPostService',
+        'appStateService'
+    ];
+
+    function homeCtrl(wpPostService, appStateService) {
+        var vm = this;
+
+        vm.post = {};
+
+        wpPostService.panelRequestPost('hero').then(function (json) {
+            vm.post.hero = json;
+        });
+
+        wpPostService.panelRequestPost('marketing').then(function (json) {
+            vm.post.marketing = json;
+        });
+
+        wpPostService.panelRequestPost('projects').then(function (json) {
+            vm.post.projects = json;
+        });
+
+    }
+})();
 (function() {
     'use strict';
 
@@ -119,35 +148,6 @@
     }
     /* @ngInject */
     function ControllerController () {
-
-    }
-})();
-(function () {
-    angular
-        .module('lefthook')
-        .controller('homeCtrl', homeCtrl);
-
-    homeCtrl.$inject = [
-        'wpPostService',
-        'appStateService'
-    ];
-
-    function homeCtrl(wpPostService, appStateService) {
-        var vm = this;
-
-        vm.post = {};
-
-        wpPostService.panelRequestPost('hero').then(function (json) {
-            vm.post.hero = json;
-        });
-
-        wpPostService.panelRequestPost('marketing').then(function (json) {
-            vm.post.marketing = json;
-        });
-
-        wpPostService.panelRequestPost('projects').then(function (json) {
-            vm.post.projects = json;
-        });
 
     }
 })();
